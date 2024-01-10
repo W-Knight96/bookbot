c069102a4b9267323e54c8f93acc3c29db2fe915
W-Knight96 <glcf360@hotmail.com>
2024-01-09 23:51:44 -0300

    Report of book finished

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        count_words(file_contents)
-        letters = count_letters(file_contents)
-        print(letters)
+    word_count = count_words(file_contents)
+    letters = count_letters(file_contents)
+    report(letters)
 
 def count_words(file_contents):
     word_list = file_contents.split()
     word_count = len(word_list)
+    return word_count
 
 def count_letters(file_contents):
     text = file_contents.lower()
@@ -17,6 +18,28 @@ def count_letters(file_contents):
             letters[letter] += 1
         else:
             letters[letter] = 1
-    return letters 
+    return letters
+
+def sort_on(d):
+    return d["num"] 
+
+def report(letters):
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    word_count = count_words(file_contents)
+    print("Report Start of Frankenstein:\n")
+    print(f"The book has {word_count} words\n ")
+
+    sorted_list = []
+    for letter in letters:
+        sorted_list.append({"letter": letter, "num": letters[letter]})
+    sorted_list.sort(reverse = True, key=sort_on)
+
+    for letter in sorted_list:
+        if not letter["letter"].isalpha():
+            continue
+        print(f"The {letter["letter"]} was found {letter["num"]} times")
+    
+    print("--- Report End ---")
 
 main()
